78ea3c0fbb4b2fc85c3e83f9cbb2b49d3790bfde
Antonio Goncalves <antonio.goncalves@gmail.com>
2022-03-04 17:01:07 +0100

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,55 @@
+### Shell script setting things up on Mac
+
+### Homebrew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+# Tools
+brew install ant
+brew install httpie
+brew install httping
+brew install htop
+brew install jq
+brew install maven
+brew install macvim
+brew install pstree
+brew install wget
+brew install tree
+brew install watch
+
+# UI
+brew cask install araxis-merge
+brew cask install iterm2
+brew cask install intellij-idea
+
+# Browsers
+brew cask install firefox
+brew cask install google-chrome
+
+# Cloud drives
+brew cask install dropbox
+brew install --cask onedrive
+
+# Cloud, containers
+brew install kubectl
+brew cask install minishift
+
+# Office stuff
+brew cask install daisydisk
+brew cask install libreoffice
+brew cask install sizeup
+brew cask install xmind
+brew cask install xtrafinder
+
+#Instant Messengers
+brew cask install skype
+brew cask install slack
+
+# Media players
+brew cask install vlc
+
+# Readers
+brew cask install kindle
+brew cask install adobe-reader
+brew cask install BBedit
+brew cask install skim # PDF
+
