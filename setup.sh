daee3a51e8792943a9e809511a04c2629a018ff7
Antonio Goncalves <antonio.goncalves@gmail.com>
2022-06-08 20:01:48 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -46,15 +46,15 @@ brew install watch
 
 # UI
 brew cask install araxis-merge
-brew cask install iterm2
-brew cask install intellij-idea
+brew install iterm2
+brew install intellij-idea
 
 # Browsers
-brew cask install firefox
-brew cask install google-chrome
+brew install firefox
+brew install google-chrome
 
 # Cloud drives
-brew cask install dropbox
+brew install dropbox
 brew install --cask onedrive
 
 # Cloud, containers
@@ -64,7 +64,7 @@ brew cask install minishift
 # Office stuff
 brew cask install daisydisk
 brew cask install libreoffice
-brew cask install sizeup
+brew install sizeup
 brew cask install xmind
 brew cask install xtrafinder
 
@@ -79,7 +79,7 @@ brew cask install vlc
 brew cask install kindle
 brew cask install adobe-reader
 brew cask install BBedit
-brew cask install skim 
+brew install skim 
 
 
 
